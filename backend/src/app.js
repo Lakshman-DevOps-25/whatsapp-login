2327b34ae707b2481c04f290d62afc953d3ffc2f
Lakshman-DevOps-25 <laxmanaraog@gmail.com>
2026-05-28 22:06:52 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,6 +19,11 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(cors({
+    origin: 'https://data-connector-frontend.onrender.com',
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
